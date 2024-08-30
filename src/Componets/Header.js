3dc9ba651a58d4ee32d97bdb32f890671d5d315f
princedonga <160412994+princedonga@users.noreply.github.com>
2024-08-30 15:40:32 +0530

    upload

--- src/Componets/Header.js
+++ src/Componets/Header.js
@@ -1,17 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link as ScrollLink } from 'react-scroll';
 import '../App.css';
 
 function Header() {
     const [showSearchBar, setShowSearchBar] = useState(false);
+    const [isSticky, setIsSticky] = useState(false);
 
     const toggleSearchBar = () => {
         setShowSearchBar(!showSearchBar);
     };
 
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.pageYOffset > 150) {
+                setIsSticky(true);
+            } else {
+                setIsSticky(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
-        <div className='position-sticky top-0 z-3 bg-white box-shadow'>
-            <section className=''>
+        <div className={`page-header ${isSticky ? 'is-sticky' : ''}`}>
+            <section>
                 <div className='container-fluid'>
                     <div className='row'>
                         <div className='col-lg-4 col-md-6 col-sm-6 col-6'>
@@ -40,7 +55,6 @@ function Header() {
                                     type="button"
                                     data-bs-toggle="offcanvas"
                                     data-bs-target="#offcanvasExample"
-
                                 ></i>
                             </div>
                         </div>
@@ -50,8 +64,8 @@ function Header() {
                     <div className='container-fluid'>
                         <div className='row'>
                             <div className='col-12 '>
-                                <div class="input-group mb-3  w-25 m-input float-end me-5">
-                                    <input type="text" className="form-control" placeholder="Search Heare" />
+                                <div className="input-group mb-3 w-25 m-input float-end me-5">
+                                    <input type="text" className="form-control" placeholder="Search Here" />
                                     <span className="input-group-text" id="basic-addon2"> <i
                                         className="fa-solid fa-magnifying-glass fs-5 cursor-pointer"
                                     ></i></span>
